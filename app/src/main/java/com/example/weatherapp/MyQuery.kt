52aab37cd1514732b2c90feb23af57887382be8d
Kudryavtsev Nikita <113279023+NikRader@users.noreply.github.com>
2024-04-07 22:55:08 +0300

    DataBase worksgit add -A!

---
+++ app/src/main/java/com/example/weatherapp/MyQuery.kt
@@ -0,0 +1,36 @@
+package com.example.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.asLiveData
+import com.example.weatherapp.DB.Item
+import com.example.weatherapp.DB.MainDb
+import com.example.weatherapp.databinding.ActivityMyQueryBinding
+
+
+class MyQuery : AppCompatActivity() {
+    lateinit var binding: ActivityMyQueryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMyQueryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val db = MainDb.getDb(this)
+        db.getDao().getAllItem().asLiveData().observe(this){ list->
+            binding.tvList.text = ""
+            list.forEach {
+                val text = "Id: ${it.id} Name: ${it.name} Price: ${it.price}\n"
+                binding.tvList.append(text)
+            }
+        }
+        binding.button2.setOnClickListener {
+            val item = Item(null,
+                binding.edName.text.toString(),
+                binding.edPrice.text.toString()
+            )
+            Thread{
+                db.getDao().insertItem(item)
+            }.start()
+        }
+    }
+}
